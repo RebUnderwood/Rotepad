a16431e1ae7d5e22d4eb09a5040fc57ae03c551f
RebUnderwood <inquiries.rb.underwood@gmail.com>
2025-10-04 18:25:52 -0400

    Added search functionality.

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -6,7 +6,8 @@ enum UnsavedState {NONE, NEW_FILE, OPENING_FILE, EXITING_PROGRAM}
 
 @onready var open_file_dialogue = $OpenFileDialog;
 @onready var write_file_dialogue = $SaveFileDialog;
-@onready var unsaved_dialogue = $UnsavedConfirmDialogue
+@onready var unsaved_dialogue = $UnsavedConfirmDialogue;
+@onready var search_dialogue = $SearchDialogue;
 
 var accepting_input: bool = true;
 var cur_file_path: String = "";
@@ -15,7 +16,9 @@ var unsaved_confirm_state: UnsavedState = UnsavedState.NONE;
 
 func _ready() -> void:
 	refresh_window_title();
-
+	unsaved_dialogue.close_requested.connect(_on_window_close_requested.bind(unsaved_dialogue));
+	search_dialogue.close_requested.connect(_on_window_close_requested.bind(search_dialogue));
+	
 func _process(_delta: float) -> void:
 	if accepting_input:
 		if Input.is_key_pressed(KEY_CTRL):
@@ -28,6 +31,8 @@ func _process(_delta: float) -> void:
 				else:
 					unsaved_confirm_state = UnsavedState.OPENING_FILE;
 					unsaved_dialogue.show();
+			elif Input.is_key_pressed(KEY_F):
+				search_dialogue.show();
 	
 func refresh_window_title() -> void:
 	var out_title : String = "";
@@ -81,6 +86,54 @@ func unsaved_followup() -> void:
 			pass;
 	unsaved_confirm_state = UnsavedState.NONE;
 
+func convert_offset_to_caret_position(origin: Vector2i, char_offset: int) -> Vector2i:
+	var out: Vector2i = Vector2i.ZERO;
+	var remaining_offset: int = origin.x + char_offset;
+	var i: int = origin.y;
+	if remaining_offset < 0:
+		i -= 1
+	var dir = char_offset / abs(char_offset);
+	while true:
+		if i >= text_edit.get_line_count():
+			i = 0;
+		elif i < 0:
+			i = text_edit.get_line_count() - 1;
+		var line_length: int = text_edit.get_line(i).strip_escapes().length();
+		if  abs(remaining_offset) > line_length:
+			remaining_offset -= (line_length * dir);
+		elif remaining_offset < 0:
+			out = Vector2i(line_length - remaining_offset - 1, i);
+			break;
+		else:
+			out = Vector2i(remaining_offset, i);
+			break;
+		i += dir;
+	return out;
+
+func search(search_dict: Dictionary) -> bool:
+	if text_edit.text.is_empty():
+		return false;
+	var search_flags: int = 0;
+	var adj_caret_pos: Vector2i = Vector2i(text_edit.get_caret_column(), text_edit.get_caret_line());
+	if search_dict.match_case:
+		search_flags += 1;
+	if search_dict.direction == Global.Direction.UP:
+		adj_caret_pos = Vector2i(text_edit.get_selection_origin_column(), text_edit.get_selection_origin_line());
+		adj_caret_pos = convert_offset_to_caret_position(adj_caret_pos, -1);
+		search_flags += 4;
+	text_edit.grab_focus();	 
+	var match_location: Vector2i = text_edit.search(search_dict.search_term, search_flags, adj_caret_pos.y, adj_caret_pos.x);
+	if match_location == Vector2i(-1, -1):
+		return false;
+	elif !search_dict.wrap && search_dict.direction == Global.Direction.UP && match_location.y > text_edit.get_caret_line():
+		return false;
+	elif !search_dict.wrap && search_dict.direction == Global.Direction.DOWN && match_location.y < text_edit.get_caret_line():
+		return false;
+	var caret_position: Vector2i = convert_offset_to_caret_position(match_location, search_dict.search_term.length());
+	text_edit.select(match_location.y, match_location.x, caret_position.y , caret_position.x);
+	text_edit.scroll_vertical = text_edit.get_scroll_pos_for_line(match_location.y);
+	return true;
+	
 func _on_save_file_dialog_file_selected(in_path: String) -> void:
 	var to_be_saved: String = text_edit.text;
 	write_file(to_be_saved, in_path);
@@ -126,16 +179,20 @@ func _on_file_menu_id_pressed(id: int) -> void:
 			else:
 				unsaved_confirm_state = UnsavedState.EXITING_PROGRAM;
 				unsaved_dialogue.show();
-			
 
+func _on_edit_menu_id_pressed(id: int) -> void:
+	match id:
+		7:
+			search_dialogue.show();
+			
+	
 
 func _on_text_edit_text_changed() -> void:
 	is_saved = false;
 	refresh_window_title();
 
-
-func _on_window_close_requested() -> void:
-	unsaved_dialogue.hide();
+func _on_window_close_requested(window: Node) -> void:
+	window.hide();
 
 func _on_cancel_button_pressed() -> void:
 	unsaved_confirm_state = UnsavedState.NONE;
@@ -148,3 +205,8 @@ func _on_dont_save_button_pressed() -> void:
 func _on_save_button_pressed() -> void:
 	unsaved_dialogue.hide();
 	save();
+
+func _on_search_dialogue_search_for(search_dict: Dictionary) -> void:
+	var match_found: bool = search(search_dict);
+	if !match_found:
+		print("NO MATCH!");
---
+++ Scenes/search_dialogue.gd
@@ -0,0 +1,34 @@
+extends Window
+
+signal search_for(search_dict: Dictionary);
+
+@onready var searchbar = $PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/Searchbar;
+@onready var find_button = $PanelContainer/MarginContainer/HBoxContainer/VBoxContainer2/FindButton;
+@onready var cancel_button = $PanelContainer/MarginContainer/HBoxContainer/VBoxContainer2/CancelButton;
+@onready var match_case_box = $PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer2/VBoxContainer/HBoxContainer/MatchCaseBox;
+@onready var wrap_box = $PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer2/VBoxContainer/HBoxContainer2/WrapBox;
+@onready var up_radio_button = $PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer2/VBoxContainer2/PanelContainer/MarginContainer/HBoxContainer/UpDirectionRadioButton;
+@onready var down_radio_button = $PanelContainer/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer2/VBoxContainer2/PanelContainer/MarginContainer/HBoxContainer/DownDirectionRadioButton;
+
+var search_dict_base: Dictionary = {
+	"search_term": "",
+	"match_case": false,
+	"wrap": false,
+	"direction": Global.Direction.DOWN,
+}
+
+func _on_cancel_button_pressed() -> void:
+	close_requested.emit();
+
+func _on_find_button_pressed() -> void:
+	if searchbar.text.is_empty():
+		return;
+	var new_search_dict = search_dict_base.duplicate();
+	new_search_dict.search_term = searchbar.text;
+	new_search_dict.match_case = match_case_box.button_pressed;
+	new_search_dict.wrap = wrap_box.button_pressed;
+	if up_radio_button.button_pressed:
+		new_search_dict.direction = Global.Direction.UP;
+	elif down_radio_button.button_pressed:
+		new_search_dict.direction = Global.Direction.DOWN;
+	search_for.emit(new_search_dict);
